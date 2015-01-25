7c95c7c807539a58b4c2d608135183925bb30fb2
Nikita Uvarov <uv.nikita@gmail.com>
2015-01-26 00:49:13 +0200

    Parse given value and currency to appropriate types.

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,17 @@ enum Currency {
 }
 
 
+impl FromStr for Currency {
+    fn from_str(s: &str) -> Option<Self> {
+        match s {
+            "UAH" => Some(Currency::UAH),
+            "USD" => Some(Currency::USD),
+            _ => None,
+        }
+    }
+}
+
+
 #[derive(Debug)]
 enum Error {
     InvalidValue,
@@ -25,7 +36,7 @@ enum Error {
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let msg = match self {
-            &Error::InvalidValue => "Invalid value. Must be float number.",
+            &Error::InvalidValue => "Invalid value. Must be positive float number.",
             &Error::InvalidCurrency => "Invalid currency.",
             &Error::ConnectionError => "Connection error.",
         };
@@ -36,6 +47,7 @@ impl fmt::Display for Error {
 
 type Exchange = (f32, Currency);
 
+
 fn render(exs: &[Exchange]) -> String {
     exs.iter().
     map(|&(ref v, ref c)| format!("{} {:?}", v, c)).
@@ -43,18 +55,33 @@ fn render(exs: &[Exchange]) -> String {
     connect(" ")
 }
 
-fn convert(strvalue: &str, currency: &str) -> Result<[Exchange; NUM_OF_CURRENCIES], Error> {
-    match FromStr::from_str(strvalue) {
-        None => Err(Error::InvalidValue),
-        Some(value) => Ok([(value, Currency::UAH)])
+
+fn convert(value_str: &str, currency_str: &str) -> Result<[Exchange; NUM_OF_CURRENCIES], Error> {
+    fn read_value(value_str: &str) -> Result<f32, Error> {
+        match FromStr::from_str(value_str) {
+            None => Err(Error::InvalidValue),
+            Some(value) if value >= 0.0 => Ok(value),
+            _ => Err(Error::InvalidValue)
+        }
     }
+
+    fn read_currency(currency_str: &str) -> Result<Currency, Error> {
+        match FromStr::from_str(currency_str) {
+            None => Err(Error::InvalidCurrency),
+            Some(currency) => Ok(currency),
+        }
+    }
+
+    let value = try!(read_value(value_str));
+    let currency = try!(read_currency(currency_str));
+    Ok([(value, currency)])
 }
 
 
 fn main() {
-    match os::args().as_slice() {
+    match &os::args()[] {
         [_, ref val, ref curr] =>
-            match convert(val.as_slice(), curr.as_slice()) {
+            match convert(&val[], &curr[]) {
                 Ok(exs) => println!("{}", render(&exs)),
                 Err(why) => println!("{}", why)
             },
